685b6fec00724a075f470683248fb4a493893d86
Mac Klosky <macklosky211@gmail.com>
2025-11-12 19:53:40 -0500

    Very basic level generation implimentation

---
+++ Scripts/board_item.gd
@@ -0,0 +1,19 @@
+class_name BoardItem extends Control
+
+enum ITEM_TYPE {TRIANGLE, RECTANGLE, POLYGON, ELLIPSE}
+
+## The type of item that this object represents.
+## Setting this variable changes the objects sprite to the respective image.
+@export var item_type : ITEM_TYPE = ITEM_TYPE.TRIANGLE:
+	set(val):
+		assert(val is ITEM_TYPE, "passed value was not an ITEM_TYPE")
+		for child in self.get_children():
+			if child is TextureRect: child.visible = false # Hide all sprites.
+		
+		# Enable relative sprite
+		match val:
+			ITEM_TYPE.TRIANGLE: $Triangle.visible = true
+			ITEM_TYPE.RECTANGLE: $Rectangle.visible = true
+			ITEM_TYPE.POLYGON: $Polygon.visible = true
+			ITEM_TYPE.ELLIPSE: $Ellipse.visible = true
+			_: assert(false, "How did you even get here.")
--- Scripts/level_generator.gd
+++ Scripts/level_generator.gd
@@ -1,1 +1,16 @@
 class_name LevelGenerator extends Node
+
+@export var desired_columns : int = 5
+
+@onready var item_grid : GridContainer = %ItemGrid
+@onready var item_blueprint : PackedScene = preload("res://Scenes/board_item.tscn")
+
+func _ready() -> void:
+	item_grid.columns = desired_columns
+	
+	for row : int in range(0, desired_columns):
+		for column : int in range(0, desired_columns):
+			var new_item : BoardItem = item_blueprint.instantiate()
+			new_item.item_type = randi_range(0,3)
+			
+			item_grid.add_child(new_item, true)
