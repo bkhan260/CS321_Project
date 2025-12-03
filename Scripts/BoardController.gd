ac0766d16a30f0f2de5b0de6eceb0c4e05b4c804
Mac Klosky <macklosky211@gmail.com>
2025-12-03 09:32:14 -0500

    Fix for 6233ed87c13a2efbbfb8ec9e4d275279eb3e4f77, I forgot to manage added files

---
+++ Scripts/BoardController.gd
@@ -0,0 +1,61 @@
+class_name BoardController extends Control
+
+enum DIFFICULTY {EASY, MEDIUM, HARD} # Must match LevelSelect.gd DIFFICULTY enum exactly to function properly...
+# And yes I know its bad programming to have the same thing in two places, I just dont care.
+
+@onready var turn_controller: TurnController = $TurnController
+@onready var score_label: RichTextLabel = $ScoreBox/ScoreLabel
+@onready var turn_label: RichTextLabel = $TurnBox/TurnLabel
+@onready var hint_label: RichTextLabel = $HintBox/HintLabel
+
+## Total score for this game
+var score : int = 0
+
+## The first item on the board that is selected when M1 is pressed down.
+var first_item : BoardItem = null
+
+## Generates a level based on the passed difficulty
+## Should be called by the LevelSelectScreen when a difficulty is selected
+## Must await BoardController.ready or else all instances will be null
+func set_difficulty(diff : DIFFICULTY) -> void:
+	await self.ready
+	$LevelGenerator.generate_level(diff)
+
+func _ready() -> void:
+	turn_controller.save_score.connect(save_score) ## Connect save score signal to function.
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		if event.is_pressed():
+			var item = get_viewport().gui_get_hovered_control()
+			if item is BoardItem:
+				first_item = item
+		else:
+			if first_item != null: ## IF ITEM is next to tile were releasing on, then swap() else: dont swap
+				var new_item = get_viewport().gui_get_hovered_control()
+				if new_item is BoardItem and first_item.pos.distance_to(new_item.pos) == 1.0: ## The distance should only be 1. if > 1 it was too far.
+					
+					## TODO: IF swap is possible, check its validity, then swap & clear valid matches.
+					
+					var temp : BoardItem.ITEM_TYPE = first_item.item_type ## Swap the two items.
+					first_item.item_type = new_item.item_type
+					new_item.item_type = temp
+					
+					## TODO ASSUMING Swap was VALID, then take a turn away + Increase score
+					score += 10
+					turn_controller.finish_turn()
+					
+					
+					score_label.text = "SCORE:\n%d" % score
+					turn_label.text = "TURNS REMAINING:\n%d" % turn_controller.turns_remaining
+			
+			first_item = null
+
+func save_score() -> void:
+	var file : FileAccess = FileAccess.open("user://highscore.save", FileAccess.READ)
+	var highscore : int = file.get_64() as int
+	file.close()
+	if score > highscore: # if new highscore, save it.
+		file = FileAccess.open("user://highscore.save", FileAccess.WRITE)
+		file.store_64(score)
+		file.close()
