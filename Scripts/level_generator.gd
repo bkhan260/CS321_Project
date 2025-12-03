6233ed87c13a2efbbfb8ec9e4d275279eb3e4f77
Mac Klosky <macklosky211@gmail.com>
2025-12-03 09:22:50 -0500

    Implimented difficulty selection & level generation for said difficulties, aka you can now play easy/medium/hard and not only easy

--- Scripts/level_generator.gd
+++ Scripts/level_generator.gd
@@ -1,15 +1,16 @@
 class_name LevelGenerator extends Node
 
-@export var desired_columns : int = 5
+@export var desired_columns : Array[int] = [5, 10, 20]
 
 @onready var item_grid : GridContainer = %ItemGrid
 @onready var item_blueprint : PackedScene = preload("res://Scenes/board_item.tscn")
 
-func _ready() -> void:
-	item_grid.columns = desired_columns
+func generate_level(difficulty : BoardController.DIFFICULTY = BoardController.DIFFICULTY.EASY) -> void:
+	var num_columns : int = desired_columns[difficulty as int]
+	item_grid.columns = num_columns
 	
-	for row : int in range(0, desired_columns):
-		for column : int in range(0, desired_columns):
+	for row : int in range(0, num_columns):
+		for column : int in range(0, num_columns):
 			var new_item : BoardItem = item_blueprint.instantiate()
 			new_item.item_type = randi_range(0,3) as BoardItem.ITEM_TYPE
 			new_item.pos = Vector2i(column, row)
