c6db91e2e1a6e4312d7209d1e3c9c0fb752f6ffe
Mac Klosky <macklosky211@gmail.com>
2025-12-03 11:24:46 -0500

    Abstracted number of different items for modularity - this can be changed to increase/decrease difficulty, but has no error checking for valid inputs

--- Scripts/level_generator.gd
+++ Scripts/level_generator.gd
@@ -2,6 +2,9 @@ class_name LevelGenerator extends Node
 
 @export var desired_columns : Array[int] = [5, 10, 20]
 
+## The number of different types of items that should be on the board
+@export var num_items : int = 3
+
 @onready var item_grid : GridContainer = %ItemGrid
 @onready var item_blueprint : PackedScene = preload("res://Scenes/board_item.tscn")
 
@@ -12,6 +15,6 @@ func generate_level(difficulty : BoardController.DIFFICULTY = BoardController.DI
 	for row : int in range(0, num_columns):
 		for column : int in range(0, num_columns):
 			var new_item : BoardItem = item_blueprint.instantiate()
-			new_item.item_type = randi_range(0,3) as BoardItem.ITEM_TYPE
+			new_item.item_type = randi_range(0, num_items) as BoardItem.ITEM_TYPE
 			new_item.pos = Vector2i(column, row)
 			item_grid.add_child(new_item, true)
