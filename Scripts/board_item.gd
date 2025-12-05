f6a0b9306ea391eccd36c914534056c22977262d
Yodahe Kidanu <yodahekidanu@mac.mynetworksettings.com>
2025-12-05 01:37:25 -0500

    Changed spirits to aquatic life

--- Scripts/board_item.gd
+++ Scripts/board_item.gd
@@ -1,10 +1,10 @@
 class_name BoardItem extends Control
 
-enum ITEM_TYPE {TRIANGLE, RECTANGLE, POLYGON, ELLIPSE}
+enum ITEM_TYPE {BLUEFISH, BROWNFISH, GREENFISH, ORANGEFISH}
 
 ## The type of item that this object represents.
 ## Setting this variable changes the objects sprite to the respective image.
-@export var item_type : ITEM_TYPE = ITEM_TYPE.TRIANGLE:
+@export var item_type : ITEM_TYPE = ITEM_TYPE.BLUEFISH:
 	set(val):
 		assert(val is ITEM_TYPE, "passed value was not an ITEM_TYPE")
 		for child in self.get_children():
@@ -12,10 +12,10 @@ enum ITEM_TYPE {TRIANGLE, RECTANGLE, POLYGON, ELLIPSE}
 		
 		# Enable relative sprite
 		match val:
-			ITEM_TYPE.TRIANGLE: $Triangle.visible = true
-			ITEM_TYPE.RECTANGLE: $Rectangle.visible = true
-			ITEM_TYPE.POLYGON: $Polygon.visible = true
-			ITEM_TYPE.ELLIPSE: $Ellipse.visible = true
+			ITEM_TYPE.BLUEFISH: $BlueFish.visible = true
+			ITEM_TYPE.BROWNFISH: $BrownFish.visible = true
+			ITEM_TYPE.GREENFISH: $GreenFish.visible = true
+			ITEM_TYPE.ORANGEFISH: $OrangeFish.visible = true
 			_: assert(false, "How did you even get here.")
 		
 		item_type = val
